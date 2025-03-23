81f15314c4692815df54817e9ff3fa57646b4816
TimShare <nbvtim43@gmail.com>
2025-03-23 23:02:07 +0300

    Добавить репозитории для работы с автомобилями, брендами и моделями в инициализацию

--- src/infrastructure/repositories/__init__.py
+++ src/infrastructure/repositories/__init__.py
@@ -1,3 +1,10 @@
 from .auth import AuthRepository, BannedRefreshTokenRepository
+from .car import BrandRepository, ModelRepository, CarRepository
 
-__all__ = ["AuthRepository", "BannedRefreshTokenRepository"]
+__all__ = [
+    "AuthRepository",
+    "BannedRefreshTokenRepository",
+    "BrandRepository",
+    "ModelRepository",
+    "CarRepository",
+]
